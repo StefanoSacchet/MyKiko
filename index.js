347947899c782bc74a3a745d4fd98de4faa981c5
StefanoSacchet <104228685+StefanoSacchet@users.noreply.github.com>
2022-05-12 21:33:14 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const User = require('./app/models/user');
 
 const port = process.env.port || 8080;
 
-app.locals.db = mongoose.connect('mongodb+srv://prova:pronti@cluster0.bj5sk.mongodb.net/Cluster0?retryWrites=true&w=majority'/*process.env.DB_URL*/)
+app.locals.db = mongoose.connect(process.env.DB_URL)
     .then(() => {
         console.log("Connected Database");
         app.listen(port, () => {
