883bb3d4020f73e048ed71fff393d61171887589
Stefano <sacchet13@gmail.com>
2022-10-26 09:12:19 +0200

    changes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const User = require('./app/models/user');
 const Razza = require('./app/models/razza');
 
+//.env file
+require('dotenv').config();
+
 const port = process.env.PORT || 8080;
 
 app.locals.db = mongoose.connect(process.env.DB_URL)
